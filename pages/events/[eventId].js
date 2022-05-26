9392fd095392bcd6af36022e28a1baed2bfee409
pandeyprafull <prafullpandey68@gmail.com>
2022-05-26 12:24:32 +0530

    add SSG in index and dynam,ic route pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,15 +1,12 @@
-import { useRouter } from "next/router"
-import { getEventById } from "../../dummy-data";
+import { getEventById, getAllEvents } from "../../helpers/api-util";
 import { Fragment } from 'react';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from "../../components/ui/error-alert";
-function EventDetailPage() {
-  const router = useRouter();
+function EventDetailPage(props) {
 
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+  const event = props.selectedEvent;
   console.log("events ---->", event)
   if (!event) {
     return (
@@ -34,4 +31,24 @@ function EventDetailPage() {
   )
 }
 
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map(event => ({ params: { eventId: event.id } }))
+  return {
+    paths: paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps(context) {
+
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+  return {
+    props: {
+      selectedEvent: event
+    }
+  }
+
+}
 export default EventDetailPage
