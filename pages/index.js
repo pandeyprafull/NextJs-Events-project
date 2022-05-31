fb9b4515a1cc77b81f7352aaaf4bcc2d6f7af423
pandeyprafull <prafullpandey68@gmail.com>
2022-05-31 11:45:24 +0530

    add HEAD SEO component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
+import Head from 'next/head';
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>NextJs Events</title>
+        <meta name="description" content="Find a lot of great events that allow you to evolve ..." />
+      </Head>
       <EventList items={props.events} />
     </div>
   )
